9148892552ac2ccdeaeb33c9ef1a5021d1a82f00
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-08-18 12:42:26 +0100

    Push

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as Font from "expo-font";
 import Home from "./Pages/Index";
-import Employee from './Pages/Employee'
-import Loans from './Pages/Loans'
-import Payroll from './Pages/Payroll'
-import More from './Pages/More'
-
+import Employee from "./Pages/Employee";
+import Loans from "./Pages/Loans";
+import Payroll from "./Pages/Payroll";
+import More from "./Pages/More";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        font1: require("./assets/fonts/Aeonik/AeonikTRIAL-Bold.otf"),
+        font2: require("./assets/fonts/Aeonik/AeonikTRIAL-BoldItalic.otf"),
+        font3: require("./assets/fonts/Aeonik/AeonikTRIAL-Light.otf"),
+        font4: require("./assets/fonts/Aeonik/AeonikTRIAL-LightItalic.otf"),
+        font5: require("./assets/fonts/Aeonik/AeonikTRIAL-Regular.otf"),
+        font6: require("./assets/fonts/Aeonik/AeonikTRIAL-RegularItalic.otf"),
+      });
+      setFontsLoaded(true);
+    }
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Profile") {
+              iconName = focused ? "person" : "person-outline";
+            }
+
+            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "#11453B",
+          inactiveTintColor: "#292A29",
+        }}
+      >
         <Tab.Screen
           name="Home"
           component={Home}
--- Pages/Index.jsx
+++ Pages/Index.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import {
-  Button,
   Image,
+  TouchableOpacity,
   ScrollView,
   StyleSheet,
   Text,
   View,
+  ImageBackground,
 } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Octicons from "react-native-vector-icons/Octicons";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Entypo from "react-native-vector-icons/Entypo";
 
 const Tab = createBottomTabNavigator();
 
 const Flex = () => {
   return (
-    <View
+    <ScrollView
       style={[
         styles.container,
         {
@@ -21,58 +27,481 @@ const Flex = () => {
         },
       ]}
     >
-      <View style={{ flex: 1, flexDirection: "row" }}>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginBottom: 15,
+        }}
+      >
         <View
           style={{
-            borderRadius: 50,
-            borderWidth: 5,
-            borderColor: "#11453B",
-            width: 85,
-            height: 85,
-            overflow: "hidden",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            width: "70%",
           }}
         >
-          <Image
-            style={{ width: 75, height: 75 }}
-            source={require("../assets/userImg.jpeg")}
-          />
+          <View
+            style={{
+              borderRadius: 50,
+              borderWidth: 5,
+              borderColor: "#11453B",
+              width: 80,
+              height: 80,
+              overflow: "hidden",
+            }}
+          >
+            <Image
+              style={{ width: 70, height: 70 }}
+              source={require("../assets/userImg.jpeg")}
+            />
+          </View>
+          <Text style={{ fontFamily: "font5", fontSize: 17, color: "#292A29" }}>
+            {" "}
+            Hello User
+          </Text>
         </View>
-        <Text> Hello User</Text>
+        <TouchableOpacity
+          style={{ width: 30, position: "relative", marginTop: -7 }}
+        >
+          <Octicons name="bell" size={23} color="#292A29" />
+          <View
+            style={{
+              backgroundColor: "red",
+              top: -2,
+              right: 7,
+              borderRadius: 20,
+              borderColor: "#edf5ff",
+              borderWidth: 2,
+              padding: 5,
+              position: "absolute",
+            }}
+          />
+        </TouchableOpacity>
       </View>
-      <View style={{ flex: 2, borderRadius: 30, backgroundColor: "#11453B" }}>
-        <Text style={{ color: "#d9ebcd", marginHorizontal: 30, marginTop: 40 }}>
+      <ImageBackground
+        source={require("../assets/bg-vector.png")}
+        style={{
+          flex: 2,
+          borderRadius: 30,
+          padding: 20,
+          backgroundColor: "#11453B",
+          overflow: "hidden",
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "font5",
+            color: "#d9ebcd",
+            marginHorizontal: 10,
+            marginTop: 20,
+          }}
+        >
           Wallet Balance
         </Text>
         <Text
           style={{
+            fontFamily: "font1",
             color: "#fff",
-            marginHorizontal: 30,
-            marginTop: 10,
-            marginBottom: 20,
+            marginHorizontal: 10,
+            marginTop: 15,
+            marginBottom: 10,
             fontSize: 22,
-            fontWeight: "800",
           }}
         >
           N15,067,789.00
         </Text>
-        <View
+        <TouchableOpacity
           style={{
-            width: "85%",
+            width: "95%",
             margin: "auto",
-            borderRadius: '50%',
-            padding: 4,
+            borderRadius: 20,
+            padding: 14,
+            backgroundColor: "#d9ebcd",
             alignSelf: "center",
+            marginTop: 12,
           }}
         >
-          <Button
-            // onPress={onPressLearnMore}
-            title="Fund Wallet"
-            color="#d9ebcd"
-          />
+          <Text
+            style={{
+              fontFamily: "font1",
+              fontWeight: 600,
+              fontSize: 12,
+              textAlign: "center",
+              color: "#11453B",
+            }}
+          >
+            Fund Wallet
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
+      <View style={{ flex: 2 }}>
+        <View>
+          <Text
+            style={{
+              fontFamily: "font5",
+              textTransform: "uppercase",
+              color: "#292a29",
+              marginTop: 30,
+              fontSize: 13,
+              letterSpacing: 1,
+            }}
+          >
+            Quick stats
+          </Text>
+        </View>
+        <View style={{ display: "flex", flexDirection: "row" }}>
+          <View
+            style={{
+              backgroundColor: "#f2f1f1",
+              padding: 3,
+              paddingVertical: 30,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 13,
+                color: "#292a29",
+                marginBottom: 10,
+              }}
+            >
+              {" "}
+              Total Payroll
+            </Text>
+            <Text
+              style={{ fontFamily: "font1", fontSize: 13, color: "#292a29" }}
+            >
+              {" "}
+              N 2,293,044,345.05
+            </Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: "#f2f1f1",
+              padding: 7,
+              paddingVertical: 30,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 13,
+                color: "#292a29",
+                marginBottom: 10,
+              }}
+            >
+              {" "}
+              Total Employee
+            </Text>
+            <Text
+              style={{ fontFamily: "font1", fontSize: 13, color: "#292a29" }}
+            >
+              {" "}
+              10,000
+            </Text>
+          </View>
         </View>
       </View>
-      <View style={{ flex: 3, backgroundColor: "green" }} />
-    </View>
+      <View style={{ flex: 1, marginBottom: 80 }}>
+        <View style={{ marginBottom: 20 }}>
+          <Text
+            style={{
+              fontFamily: "font5",
+              textTransform: "uppercase",
+              color: "#292a29",
+              marginTop: 30,
+              fontSize: 13,
+              letterSpacing: 1,
+            }}
+          >
+            Quick Actions
+          </Text>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            borderWidth: 1,
+            borderRadius: 20,
+            borderColor: "#dfdfdf",
+            paddingHorizontal: 20,
+            paddingVertical: 35,
+          }}
+        >
+          <TouchableOpacity style={{ width: 85 }}>
+            <View
+              style={{
+                backgroundColor: "#f0f7eb",
+                borderRadius: 20,
+                width: 80,
+                margin: "auto",
+                padding: 25,
+              }}
+            >
+              <FontAwesome5
+                name={"money-bill-wave-alt"}
+                size={25}
+                color="#11453b"
+                style={{ textAlign: "center" }}
+              />
+            </View>
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 11,
+                color: "#515251",
+                marginTop: 12,
+                marginBottom: 10,
+                textAlign: "center",
+              }}
+            >
+              Pay Salaries
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ width: 85 }}>
+            <View
+              style={{
+                backgroundColor: "#ffedec",
+                borderRadius: 20,
+                width: 80,
+                margin: "auto",
+                padding: 25,
+              }}
+            >
+              <AntDesign
+                name={"adduser"}
+                size={25}
+                color="#ea4e4b"
+                style={{ textAlign: "center" }}
+              />
+            </View>
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 11,
+                color: "#515251",
+                marginTop: 12,
+                marginBottom: 10,
+                textAlign: "center",
+              }}
+            >
+              Add Employee
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ width: 85 }}>
+            <View
+              style={{
+                backgroundColor: "#f0eeff",
+                borderRadius: 20,
+                width: 80,
+                margin: "auto",
+                padding: 25,
+              }}
+            >
+              <FontAwesome5
+                name={"envelope-open-text"}
+                size={25}
+                color="#5542cf"
+                style={{ textAlign: "center" }}
+              />
+            </View>
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 11,
+                color: "#515251",
+                marginTop: 12,
+                marginBottom: 10,
+                textAlign: "center",
+              }}
+            >
+              Send Payslip
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ width: 85 }}>
+            <View
+              style={{
+                backgroundColor: "#fff2db",
+                borderRadius: 20,
+                width: 80,
+                padding: 25,
+              }}
+            >
+              <MaterialCommunityIcons
+                name={"file-edit-outline"}
+                size={25}
+                color="#f59e0b"
+                style={{ textAlign: "center" }}
+              />
+            </View>
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 11,
+                color: "#515251",
+                marginTop: 12,
+                marginBottom: 10,
+                textAlign: "center",
+              }}
+            >
+              Edit Salaries
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ width: 85 }}>
+            <View
+              style={{
+                backgroundColor: "#e5f6fe",
+                borderRadius: 20,
+                width: 80,
+                padding: 25,
+              }}
+            >
+              <FontAwesome5
+                name={"hand-holding-usd"}
+                size={25}
+                color="#116487"
+                style={{ textAlign: "center" }}
+              />
+            </View>
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 11,
+                color: "#515251",
+                marginTop: 12,
+                marginBottom: 10,
+                textAlign: "center",
+              }}
+            >
+              Get A loan
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ width: 85 }}>
+            <View
+              style={{
+                backgroundColor: "#e7fbef",
+                borderRadius: 20,
+                width: 80,
+                padding: 25,
+              }}
+            >
+              <FontAwesome5
+                name={"telegram-plane"}
+                size={25}
+                color="#219653"
+                style={{ textAlign: "center" }}
+              />
+            </View>
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 11,
+                color: "#515251",
+                marginTop: 12,
+                marginBottom: 10,
+                textAlign: "center",
+              }}
+            >
+              Make A Transfer
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ width: 85 }}>
+            <View
+              style={{
+                backgroundColor: "#EAF6FC",
+                borderRadius: 20,
+                width: 80,
+                padding: 25,
+              }}
+            >
+              <FontAwesome5
+                name={"phone-alt"}
+                size={25}
+                color="#32A7E2"
+                style={{ textAlign: "center" }}
+              />
+            </View>
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 11,
+                color: "#515251",
+                marginTop: 12,
+                marginBottom: 10,
+                textAlign: "center",
+              }}
+            >
+              Airtime
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ width: 85 }}>
+            <View
+              style={{
+                backgroundColor: "#F8EDF9",
+                borderRadius: 20,
+                width: 80,
+                padding: 25,
+              }}
+            >
+              <Entypo
+                name={"signal"}
+                size={25}
+                color="#B548C6"
+                style={{ textAlign: "center" }}
+              />
+            </View>
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 11,
+                color: "#515251",
+                marginTop: 12,
+                marginBottom: 10,
+                textAlign: "center",
+              }}
+            >
+              Data
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ width: 85 }}>
+            <View
+              style={{
+                backgroundColor: "#FFF3E6",
+                borderRadius: 20,
+                width: 80,
+                padding: 25,
+              }}
+            >
+              <FontAwesome5
+                name={"bolt"}
+                size={25}
+                color="#FF8700"
+                style={{ textAlign: "center" }}
+              />
+            </View>
+            <Text
+              style={{
+                fontFamily: "font5",
+                fontSize: 11,
+                color: "#515251",
+                marginTop: 12,
+                marginBottom: 10,
+                textAlign: "center",
+              }}
+            >
+              Electricity
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
